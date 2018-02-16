a99b21c4e59dd43bf7c854937915e478361faedc
Stephanus Venter <stephanus.venter@kurtosys.com>
2018-02-16 13:26:14 +0200

    Added sample porps to demonstrate the class definition in React when using typescript

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,11 +2,11 @@ import * as React from 'react';
 import './App.css';
 
 interface Props {
-
+	samplePropProp?: string;
 }
 
 interface State {
-
+	sampleStateProp?: string;
 }
 
 export class App extends React.Component<Props, State> {
